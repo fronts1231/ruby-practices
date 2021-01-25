1f64384528d0253146900524aacb348c6c7159ad
fronts1231 <takumi.maeomote1231@gmail.com>
2021-01-25 13:11:52 +0900

    0125modify

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -16,39 +16,40 @@ class Option
   end
 end
 
-@option = Option.new
+option = Option.new
 
 files = []
 ARGV.each { |file| files << file }
 files.map! { |file| File.absolute_path(file) }
 
-if ARGV.size >= 1
-  @inputs = files.map! { |file| File.readlines(file) }
+inputs = if ARGV.size >= 1
+           files.map! { |file| File.readlines(file) }
+         else
+           [$stdin.readlines]
+         end
+
+lines = inputs.map(&:size)
+items = inputs.map { |input| input.join.gsub(/\s{2,}/, "\s").count("/\s\n/") }
+bytes = inputs.map { |input| input.join.size }
+
+if ARGV.size >= 2
+  lines << lines.sum
+  items << items.sum
+  bytes << bytes.sum
   names = ARGV.push('total')
 else
-  @inputs = [$stdin.readlines]
-  names = ARGV.fill(nil, @inputs.size + 1, 0)
+  names = ARGV.fill(nil, inputs.size, 0)
 end
-
-lines = @inputs.map(&:size)
-items = @inputs.map { |input| input.join.gsub(/\s{2,}/, "\s").count("/\s\n/") }
-bytes = @inputs.map { |input| input.join.size }
 
 max_size = [lines.sum.to_s, items.sum.to_s, bytes.sum.to_s].map(&:size).max
 
 contents = [
-  lines.push(lines.sum).map { |line| "#{line.to_s.rjust(max_size + 5)}\s" },
-  items.push(items.sum).map { |item| "#{item.to_s.rjust(max_size + 4)}\s" },
-  bytes.push(bytes.sum).map { |byte| "#{byte.to_s.rjust(max_size + 4)}\s" },
+  lines.push.map { |line| "#{line.to_s.rjust(max_size + 5)}\s" },
+  items.push.map { |item| "#{item.to_s.rjust(max_size + 4)}\s" },
+  bytes.push.map { |byte| "#{byte.to_s.rjust(max_size + 4)}\s" },
   names
 ]
 
-contents = [contents.first, contents.last] if @option.has?(:lines)
+contents = [contents.first, contents.last] if option.has?(:lines)
 
-contents_transposed = contents.transpose.map(&:join)
-
-if ARGV.size >= 3
-  puts contents_transposed
-else
-  puts contents_transposed.pop(1)
-end
+puts contents.transpose.map(&:join)
